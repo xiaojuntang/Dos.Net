ad42efba2e5334d63eb5b1e17162c5beb1decbec
417281862@qq.com <417281862@qq.com>
2016-06-11 21:50:09 +0800

    DAL

---
+++ Zxxk.Dos.Web/Zxxk.Dos.DataAccess/AnswerService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zxxk.Dos.DataModel;
+
+namespace Zxxk.Dos.DataAccess
+{
+    public class AnswerService : Repository<Answer>
+    {
+        public AnswerService()
+        {
+
+        }
+    }
+}
---
+++ Zxxk.Dos.Web/Zxxk.Dos.UnitTest/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Zxxk.Dos.DataModel;
+using Zxxk.Dos.DataAccess;
+
+namespace Zxxk.Dos.UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Answer an = new Answer();
+            an.HomeWorkID = 1;
+            an.OpenID = "eeeeeeeeeeeeeeee";
+
+            int a1 = AnswerService.Insert(an);
+        }
+    }
+}
